0540034afcf697cd921a6eb82ed6c7d74760e7f0
Nedhen <nedheng.com>
2024-04-02 03:01:20 +0530

    added balance in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,15 +2,29 @@ import { useEffect, useState } from "react";
 import { Balance } from "../components/Balance";
 import { TopBar } from "../components/TopBar";
 import { Users } from "../components/Users";
+import axios from "axios";
 
 
 export default function Dashboard(){
    
     
+    
+    const [balance,setBalance] = useState()
+    useEffect(()=>{
+        axios.get("http://localhost:3000/api/v1/account/balance",{
+                    headers:{
+                            Authorization: "Bearer "+ localStorage.getItem('token')
+                        }
+                }).then((res)=>{
+                    setBalance(res.data.balance)
+                })
+    },[])
+    
+    
     return (
         <div className="h-screen bg-teal-100">
-            <TopBar user={"nedhen"}></TopBar>
-            <Balance balance={500}></Balance>
+            <TopBar user={name}></TopBar>
+            <Balance balance={balance}></Balance>
             <div className="">
                 <Users></Users>
             </div>
